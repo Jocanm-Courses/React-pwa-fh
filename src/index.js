5fcb15a11755e712ab1bb4e092481e717580ab54
Jose Angarita <amjose09@gmail.com>
2022-04-22 11:37:16 -0500

    workbox implementado

--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ import {
   CalendarApp
 } from './CalendarApp';
 
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
 import './styles.css';
 
 ReactDOM.render( <
   CalendarApp / > ,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
