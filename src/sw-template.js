f3a19736a5364f160277f79922fd9165489d9aa5
Jose Angarita <amjose09@gmail.com>
2022-04-24 14:09:37 -0500

    optimización implementada

--- src/sw-template.js
+++ src/sw-template.js
@@ -12,24 +12,26 @@ const { registerRoute } = workbox.routing
 const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies
 const { BackgroundSyncPlugin } = workbox.backgroundSync
 
+const netWorkFirst = [
+    '/api/auth/renew',
+    '/api/events'
+]
+
+const cacheFirst = [
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
+]
 
 registerRoute(
-    new RegExp('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'), 
+    ({request, url}) => cacheFirst.includes(url.pathname), 
     new CacheFirst()
 )
 
 registerRoute(
-    new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'), 
-    new CacheFirst()
-)
-
-registerRoute(
-    new RegExp('http://localhost:4000/api/auth/renew'), 
-    new NetworkFirst()
-)
-
-registerRoute(
-    new RegExp('http://localhost:4000/api/events'), 
+    ({request, url}) => {
+        console.log(url)
+        return netWorkFirst.includes(url.pathname)
+    }, 
     new NetworkFirst()
 )
 
